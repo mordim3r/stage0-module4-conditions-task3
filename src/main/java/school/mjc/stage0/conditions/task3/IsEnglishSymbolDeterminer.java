dcb84c42cbfc7cd9ff283d3710fecd9bf60a2419
mordim3r <valent1n.bazylev@gmai.com>
2026-06-05 15:54:43 +0300

    3

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (symbol=='E' ||symbol=='N'||symbol=='G'||symbol=='e' ||symbol=='n'||symbol=='g'){
+            System.out.println("English");
+        } else System.out.println("Non English");
     }
 }
