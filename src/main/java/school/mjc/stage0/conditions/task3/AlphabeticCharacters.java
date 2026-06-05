dfa932a6710cfb189a2c3848d0bf6e5fc6dc1fb7
mordim3r <valent1n.bazylev@gmail.com>
2026-06-05 21:23:01 +0300

    add

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char ch = Character.toLowerCase(character);
+
+        if (ch < 'a' || ch > 'z') {
+            System.out.println("wrong alphabet!");
+        } else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000){
+            System.out.println("taxes - 15%");
+        } else if (salary <= 20000) {
+            System.out.println("taxes - 18%");
+            
+        } else if (salary>20000) {
+            System.out.println("taxes - 20%");
+
+        } else System.out.println("wrong input!");
     }
 }
